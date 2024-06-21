869e303cd9002657d427130ff47f5319da6e0a4d
codebender828 <codebender828@gmail.com>
2024-06-21 21:38:42 +0300

    fix: globally define process

--- apps/explorer/layouts/default.vue
+++ apps/explorer/layouts/default.vue
@@ -3,3 +3,14 @@
     <slot />
   </main>
 </template>
+
+<script type="module" lang="ts">
+// Global node polyfill.
+
+import { Buffer } from "buffer";
+import process from "process";
+
+window.global = window;
+window.Buffer = Buffer;
+window.process = process;
+</script>
