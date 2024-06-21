e4c876000c787d0fd10da1c2f7b5cb463e4b2775
codebender828 <codebender828@gmail.com>
2024-06-21 21:44:52 +0300

    feat: import glbals

--- apps/explorer/layouts/default.vue
+++ apps/explorer/layouts/default.vue
@@ -4,13 +4,6 @@
   </main>
 </template>
 
-<script type="module" lang="ts">
-// Global node polyfill.
-
-import { Buffer } from "buffer";
-import process from "process";
-
-window.global = window;
-window.Buffer = Buffer;
-window.process = process;
+<script setup lang="ts">
+import "~/lib/globals";
 </script>
---
+++ apps/explorer/lib/globals.ts
@@ -0,0 +1,8 @@
+// Global node polyfill.
+
+import { Buffer } from "buffer";
+import process from "process";
+
+window.global = window;
+window.Buffer = Buffer;
+window.process = process;
