bfb547ae8294eb36af3bcf37072cdfd2cebb7a4e
codebender828 <codebender828@gmail.com>
2024-06-23 18:52:01 +0300

    feat: add github repo link

--- apps/explorer/components/molecules/top-navigation.vue
+++ apps/explorer/components/molecules/top-navigation.vue
@@ -6,8 +6,17 @@
       <NuxtLink to="/" class="text-black">
         <MoleculesLogo class="h-7 text-black" />
       </NuxtLink>
+
+      <NuxtLink
+        href="https://github.com/mirrorworld-universe/cascade-protocol"
+        target="_blank"
+      >
+        <GithubLogoIcon class="ml-auto h-5 w-5 text-black" />
+      </NuxtLink>
     </MoleculesContainer>
   </nav>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { GithubLogoIcon } from "@radix-icons/vue";
+</script>
