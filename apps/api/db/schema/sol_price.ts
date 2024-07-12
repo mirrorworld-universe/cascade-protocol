9f29d355dee1377ed0670ecb913818f3d13645d2
codebender828 <codebender828@gmail.com>
2024-07-12 18:09:29 +0100

    chore: test remove all columnsd from migration

--- apps/api/db/schema/sol_price.ts
+++ apps/api/db/schema/sol_price.ts
@@ -1,7 +1,5 @@
-import { date, numeric, pgTable, uuid } from "drizzle-orm/pg-core";
+import { date, numeric, pgTable, serial } from "drizzle-orm/pg-core";
 
 export const sol_price = pgTable("sol_price", {
-  id: uuid("id").defaultRandom(),
-  date: date("date").notNull(),
-  value: numeric("value").notNull(),
+  id: serial("id").primaryKey(),
 });
--- apps/api/db/schema/test_table.ts
+++ apps/api/db/schema/test_table.ts
@@ -1,6 +1,5 @@
 import { pgTable, serial, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const test_table = pgTable("test_table", {
-  id: uuid("id").defaultRandom(),
-  name: varchar("name", { length: 256 }),
+  id: serial("id").primaryKey(),
 });
